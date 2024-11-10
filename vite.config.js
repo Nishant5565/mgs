ee6a0b8c73f286ae0b0e4ac96fbfda60a43bae7c
Nishant <ntih5565@gmail.com>
2024-11-10 19:40:23 +0530

    hosting

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/mgs',
+  base : '/mgs/',
   server: {
     historyApiFallback: true, // handles all routes as fallback
   },
