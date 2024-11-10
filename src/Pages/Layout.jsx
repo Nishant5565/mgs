40bd6c15cfc701c1cd3c2ab696c93e4158e54769
Nishant <ntih5565@gmail.com>
2024-11-10 20:19:21 +0530

    Readme

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import SideNav from '../Components/SideNav/SideNav'
-import TopSection from '@/Components/TopSection/TopSection'
+import TopSection from '../Components/TopSection/TopSection'
 const Layout = () => {
   return (
        <div className="flex">
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   base : '/mgs',
-  // resolve: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, './src'),
-  //   },
-  // },
+
 })
